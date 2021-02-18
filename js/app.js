66a219cfbceb5781b008502354c35344bac85b6e
saeedhassansolangi <saeedhassansolangi@gmail.com>
2021-02-18 09:01:08 +0500

    completed portfolio section

--- js/app.js
+++ js/app.js
@@ -4,20 +4,32 @@ fetch('https://api.github.com/users/saeedhassansolangi/starred')
   })
   .then((repos) => {
     let card = repos.map((repo) => {
-      return `<div class="col-md-3">
-        <div class="card mb-3" class="cards" style="background-color: #272727;">
-          <div class="row g-0">
-            <div class="col-md-8">
+      return `
+        <div class="col-md-5">
+          <div class="card mb-3" style="background-color: #272727;">
               <div class="card-body">
-                <h5 class="card-title">${repo.name}</h5>
-                <p class="card-text">${repo.description}</p>
-                <p class="card-text"><small class="text-muted">Last updated ${repo.created_at}</small></p>
+                <h5 class="card-title">${repo.name.toUpperCase()}</h5>
+                <p class="card-text">${repo.description.slice(0, 50)}...</p>
+                <p class="card-text">
+                <small class="text-muted">Created at ${new Date(
+                  repo.created_at
+                ).toLocaleString()}</small>
+                <small class="text-muted">Last Updated at ${new Date(
+                  repo.updated_at
+                ).toLocaleString()}</small>
+                </p>
+                <div >
+                <a href="${
+                  repo.html_url
+                }" target="_blank"  class="card-link">View Project On Github </a> 
+                <a href="${
+                  repo.homepage
+                }" target="_blank"  class="card-link">View Project Live </a> 
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      `;
+          
+      </div>`;
     });
 
     document.querySelector('#portfolio .row').innerHTML = card;
